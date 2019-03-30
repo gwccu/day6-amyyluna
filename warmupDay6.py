adb3ac33faa6ec7fc0df52670fe8bf3ad93ea232
amyyluna <afrancis@beaconschool.org>
2019-03-30 14:44:51 +0000

    while loops

--- warmupDay6.py
+++ warmupDay6.py
@@ -1,1 +1,13 @@
 # File name: warmupDay6.py
+answer = 0
+while answer <= 8:
+    print(answer)
+    answer += 2
+answer = 0
+while answer <= 144:
+    print(answer)
+    answer += 2**2
+answer = 0
+while answer <= 80:
+     print(answer)
+     answer += 2*2
